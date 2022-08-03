05d18a809294511e9582f89ad12384fe0ee8b805
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-08-03 19:49:32 -0300

    Att

--- OO/OO2/Filme.py
+++ OO/OO2/Filme.py
@@ -8,8 +8,8 @@ class Programa:
     def like(self):
         return self._like
 
-    def dar_like(self):
-        self._like += 1
+    def dar_like(self, qtt=1):
+        self._like += qtt
 
     @property
     def ano(self):
@@ -50,12 +50,37 @@ class Serie(Programa):
         return f'{self.nome} {self.ano} {self.temporadas} {self.like}'
 
 
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.__programas = programas
+
+    def __getitem__(self, item):
+        return self.programas[item]
+
+
+    @property
+    def programas(self):
+        return self.__programas
+
+    @property
+    def tamanho(self):
+        return len(self.programas)
+
+
+
+
+
 s1 = Serie("Gotham", 2019, 2)
 f2 = Filme('Filme2', 2022, 322)
 f1 = Filme("Filme1", 2022, 233)
+f3 = Filme("Todo mundo em panico", 1999, 100)
+s2 = Serie("Demolidor", 2016, 5)
+s2.dar_like(22)
 
+filmes_e_series = [f2, f1, s1, s2, f3]
+pl_fim_de_semana = Playlist("Fim de Semana", filmes_e_series)
 
-filmes_e_series = [f2, f1, s1]
-
-for programa in filmes_e_series:
-    print(programa)
+for count, programa in enumerate(pl_fim_de_semana):
+    print(f'{pl_fim_de_semana.nome} {count+1}° {programa}')
+print(f3 in pl_fim_de_semana)
